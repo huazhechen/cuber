e6a2f836a30887d203e8adca17cdf26385b34672
huazhechen <37705123@qq.com>
2018-10-04 11:07:01 +0800

    use default slider

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.css";
 import "typeface-roboto";
 import "material-design-icons/iconfont/material-icons.css";
-import VueSlider from "./vue/slider";
 
 Vue.use(Vuetify);
-Vue.component('vue-slider', VueSlider)
 
 new Vue({
   el: "#app",
