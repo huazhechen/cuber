ed87349a6bcbf1840a526dcca2d7dd0a50f4e936
huazhechen <huazhechen@huawei.com>
2018-10-08 14:26:40 +0800

    no dev tool

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,53 @@
 var path = require("path");
 var webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/main.ts",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/dist/",
-    filename: "main.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
-      },
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader"
-      },
-      {
-        test: /\.html?$/,
-        loader: "text-loader"
-      },
-      {
-        test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-        loader: "url-loader"
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      vue$: "vue/dist/vue.esm.js"
+module.exports = (env, argv) => ({
+    entry: "./src/main.ts",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: "/dist/",
+        filename: "main.js"
     },
-    extensions: ["*", ".js", ".ts", ".json"]
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true,
-    disableHostCheck: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: "#eval-source-map",
-  plugins: [
-    new webpack.DllReferencePlugin({
-      context: __dirname,
-      manifest: require('./dist/manifest.json')
-    })
-  ]
-};
-
-if (process.env.NODE_ENV === "production") {
-  module.exports.devtool = "#source-map";
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ]);
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["vue-style-loader", "css-loader"]
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.html?$/,
+                loader: "text-loader"
+            },
+            {
+                test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+                loader: "url-loader"
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.esm.js"
+        },
+        extensions: ["*", ".js", ".ts", ".json"]
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true,
+        disableHostCheck: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: argv.mode === 'production' ? "" : "#cheap-module-eval-source-map",
+    plugins: [
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require('./dist/manifest.json')
+        })
+    ]
+});
