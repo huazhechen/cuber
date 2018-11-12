89a071b0f38a0c4fb41f03c581940fdaf3309158
huazhechen <37705123@qq.com>
2018-11-12 21:32:48 +0800

    camera in movie

--- src/vue/AppMenu/index.ts
+++ src/vue/AppMenu/index.ts
@@ -33,26 +33,6 @@ export default class AppMenu extends Vue {
     this.$emit("input", false);
   }
   
-  snap() {
-    let content = this.game.canvas.toDataURL("image/png");
-    let parts = content.split(";base64,");
-    let type = parts[0].split(":")[1];
-    let raw = window.atob(parts[1]);
-    let length = raw.length;
-    let data = new Uint8Array(length);
-    for (let i = 0; i < length; ++i) {
-      data[i] = raw.charCodeAt(i);
-    }
-    let blob = new Blob([data], { type: type });
-
-    let link = document.createElement("a");
-    let evt = document.createEvent("MouseEvents");
-    evt.initEvent("click", false, false);
-    link.download = "cuber.png";
-    link.href = URL.createObjectURL(blob);
-    link.dispatchEvent(evt);
-  }
-  
   reset() {
     let storage = window.localStorage;
     storage.clear();
--- src/vue/MoviePanel/index.ts
+++ src/vue/MoviePanel/index.ts
@@ -182,6 +182,7 @@ export default class MoviePanel extends Vue {
     link.dispatchEvent(click);
   }
 
+  mode: string = "snap";
   recording: boolean = false;
   film() {
     this.game.duration = 30 - this.speed * 5;
@@ -191,6 +192,38 @@ export default class MoviePanel extends Vue {
     this.encoder.start();
     this.encoder.add();
     this.game.twister.twist("-" + this.action + "-", false, 1, this.save, false);
+  }
+
+  snap() {
+    this.renderer.render(this.game.scene, this.camera);
+    let content = this.renderer.domElement.toDataURL("image/png");
+    let parts = content.split(";base64,");
+    let type = parts[0].split(":")[1];
+    let raw = window.atob(parts[1]);
+    let length = raw.length;
+    let data = new Uint8Array(length);
+    for (let i = 0; i < length; ++i) {
+      data[i] = raw.charCodeAt(i);
+    }
+    let blob = new Blob([data], { type: type });
+
+    let link = document.createElement("a");
+    let evt = document.createEvent("MouseEvents");
+    evt.initEvent("click", false, false);
+    link.download = "cuber.png";
+    link.href = URL.createObjectURL(blob);
+    link.dispatchEvent(evt);
+  }
+
+  generate() {
+    switch (this.mode) {
+      case "snap":
+        this.snap();
+        break;
+      case "film":
+        this.film();
+        break;
+    }
   }
 
   share() {
