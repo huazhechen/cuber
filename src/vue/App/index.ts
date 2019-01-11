3cca41216f1a09c4c19a482f11a476d5cfaa6c03
huazhechen <37705123@qq.com>
2019-01-11 22:29:47 +0800

    min width

--- src/vue/App/index.ts
+++ src/vue/App/index.ts
@@ -25,33 +25,31 @@ export default class App extends Vue {
 
   menu: boolean = false;
 
-  width: string = "100%";
+  width: number = 0;
 
   fixed: boolean = false;
 
   resize() {
-    let el = this.$el;
-    if (window.innerWidth > window.innerHeight / (4 / 3)) {
-      if (this.width == "100%") {
-        this.width = window.innerHeight / (16 / 9) + "px";
-        el.style.width = this.width;
-        this.$nextTick(this.resize);
-        return;
-      }
-    } else {
-      if (this.width != "100%") {
-        this.width = "100%";
-        el.style.width = this.width;
-        this.$nextTick(this.resize);
-        return;
-      }
-    }
-
     if (this.$refs.cuber instanceof HTMLElement && this.$refs.panel instanceof HTMLElement) {
       let cuber = this.$refs.cuber;
       let panel = this.$refs.panel;
+      if (cuber.clientWidth > window.innerWidth) {
+        this.$el.style.width = window.innerWidth + "px";
+        cuber.style.width = window.innerWidth + "px";
+        panel.style.width = window.innerWidth + "px";
+        this.$nextTick(this.resize);
+        return;
+      }
+      let width = Math.min(window.innerWidth, this.width);
+      if (cuber.clientWidth != width) {
+        this.$el.style.width = this.width + "px";
+        cuber.style.width = this.width + "px";
+        panel.style.width = this.width + "px";
+        this.$nextTick(this.resize);
+        return;
+      }
       let panelHeight = panel.clientHeight;
-      let cuberHeight = window.innerHeight - panelHeight;
+      let cuberHeight = Math.max(window.innerHeight - panelHeight, 0);
       cuber.style.height = cuberHeight + "px";
       panel.style.position = "fixed";
       panel.style.width = cuber.clientWidth + "px";
@@ -78,6 +76,14 @@ export default class App extends Vue {
     document.onfullscreenchange = () => {
       this.$nextTick(this.resize);
     };
+
+    if (window.innerWidth > window.innerHeight / (4 / 3)) {
+      this.width = Math.floor(Math.max(window.innerHeight / (16 / 9), 320));
+      this.$el.style.width = this.width + "px";
+      this.$nextTick(this.resize);
+    } else {
+      this.width = window.innerWidth;
+    }
   }
 
   @Watch("option.mode")
