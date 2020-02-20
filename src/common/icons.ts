eeaf9279e03d1f31789073087e1470ced435e428
huazhechen <37705123@qq.com>
2020-02-20 21:51:42 +0800

    use vuetify

--- src/common/icons.ts
+++ src/common/icons.ts
@@ -1,22 +1,12 @@
 export var ICONS = {
   backspace: require("../resource/backspace.svg"),
   mirror: require("../resource/mirror.svg"),
+  colorize: require("../resource/colorize.svg"),
   layer1: require("../resource/layer1.svg"),
   layer2: require("../resource/layer2.svg"),
   layer3: require("../resource/layer3.svg"),
-  cfop0: require("../resource/cfop0.svg"),
   cfop1: require("../resource/cfop1.svg"),
   cfop2: require("../resource/cfop2.svg"),
   hollow: require("../resource/hollow.svg"),
-  lock: require("../resource/lock.svg"),
-  menu: require("../resource/menu.svg"),
-  casino: require("../resource/casino.svg"),
-  close: require("../resource/close.svg"),
-  school: require("../resource/school.svg"),
-  movie: require("../resource/movie.svg"),
-  tune: require("../resource/tune.svg"),
-  code: require("../resource/code.svg"),
-  games: require("../resource/games.svg"),
-  crop: require("../resource/crop.svg"),
-  table: require("../resource/table.svg")
+  lock: require("../resource/lock.svg")
 };
